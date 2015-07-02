79dae4dcea4413832373b6327840bcb7e5c7579e
Hirokazu <saigyoji205@gmail.com>
2015-07-02 14:38:14 +0900

    Fix NullPointer (SelectWindow: 38)

--- src/FileAccess.java
+++ src/FileAccess.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 
 public class FileAccess {
 
-    File filePath;
+    private static File filePath;
 
     public void setFilePath(File filePath) {
         this.filePath = filePath;
